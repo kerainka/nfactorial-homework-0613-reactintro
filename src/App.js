cac5bff6c759877b1e8be99074551f0de12bc87f
Akerke <bekturovaakerke@gmail.com>
2022-06-14 09:31:47 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { format } from 'date-fns'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="d-flex p-4 flex-row justify-content-between pageHeader">
+        <img src="https://getbootstrap.com/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" />
+        <div className="headerDate">{format(new Date(), 'dd.MM.yyyy hh:mm')}</div>
+      </div>
+      <div className="d-flex imgContainer">
+        <div className="d-flex flex-column flex-grow-1 img1 imgDiv justify-content-center">
+          Hey
+        </div>
+        <div className="d-flex flex-column flex-grow-1 img2 imgDiv justify-content-center">
+          Let's
+        </div>
+        <div className="d-flex flex-column flex-grow-1 img3 imgDiv justify-content-center">
+          Give
+        </div>
+        <div className="d-flex flex-column flex-grow-1 img4 imgDiv justify-content-center">
+          All
+        </div>
+        <div className="d-flex flex-column flex-grow-1 img5 imgDiv justify-content-center">
+          You Can
+        </div>
+      </div>
     </div>
   );
 }
